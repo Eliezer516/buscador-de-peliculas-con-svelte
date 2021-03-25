cac481aa7e14fb427434664e99e6c4cbe3c4c6a8
Eliezer516 <ybarretomaterano@gmail.com>
2021-03-24 21:11:49 -0400

    Agregados estilos y creadas las cards

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,22 @@
 </script>
 
 <main>
-	<div class="w-auto mt-3 bg-white md:rounded-xl">
-		<h1 class="text-5xl text-center ">Buscador de peliculas</h1>
-	</div>
-	<div class="bg-white md:rounded-xl w-full h-full p-8">
-		
+	<div class="bg-white md:rounded-xl w-full h-full p-4">
+		<div class="flex justify-center items-center flex-col">
+			<h1 class="text-5xl text-center mb-4">Buscador de peliculas</h1>
+			<input type="text" class="border border-black rounded outline-none p-2">
+		</div>
+		<div class="grid grid-cols-4 gap-4">
+			<div class="card border-2 border-black rounded-md">
+			  <header class="p-2">
+			    <h1 class="text-2xl">Avengers infiniti war</h1>
+			  </header>
+			  <img src="https://fakeimg.pl/300/" alt="hola" class="border-t-2 border-b-2 border-black">
+			  <footer class="p-2">
+			    <button class="bg-indigo-500 px-4 text-white rounded hover:bg-indigo-400">ver</button>
+			  </footer>
+			</div>
+		</div>
 	</div>
 </main>
 
