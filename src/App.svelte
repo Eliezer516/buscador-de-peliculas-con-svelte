b070ff72fe10f20a5b6d3df4b6ee4efcab47fdac
Eliezer516 <ybarretomaterano@gmail.com>
2021-03-24 20:47:56 -0400

    repo iniciado

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<div class="w-auto mt-3 bg-white md:rounded-xl">
+		<h1 class="text-5xl text-center ">Buscador de peliculas</h1>
+	</div>
+	<div class="bg-white md:rounded-xl w-full h-full p-8">
+		
+	</div>
+</main>
+
+<style>
+
+</style>
